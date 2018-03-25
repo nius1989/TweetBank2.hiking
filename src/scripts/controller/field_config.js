117f51d6d6bc5d710c78854ae3ab365ff46ebad1
shoenneil@gmail.com <shoenneil@gmail.com>
2018-03-25 08:21:13 -0400

    update to core2

--- src/scripts/controller/field_config.js
+++ src/scripts/controller/field_config.js
@@ -1,7 +1,7 @@
 /**
  * Created by Shuo on 11/2/2017.
  */
-var solrURL = "http://localhost:9984/solr/hiking_core/select?";
+var solrURL = "http://localhost:9984/solr/hiking_core2/select?";
 //keys for solr query.
 var field_tweet_id = "id";
 var field_urls = "urls";
